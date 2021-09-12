d7d0915b34e765744ecb268566fb7689003b6c97
E <e@pastel.codes>
2021-09-12 21:36:29 +0100

    Add linter and formatter

--- .github/ISSUE_TEMPLATE/feature_request.md
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -4,7 +4,6 @@ about: Suggest an idea for this project
 title: 'feature: '
 labels: enhancement
 assignees: aurora-dot
-
 ---
 
 **Is your feature request related to a problem? Please describe.**
--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
@@ -12,27 +17,33 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
-renderer.setClearColor( 0x282a36 );
+renderer.setClearColor(0x282a36);
 
-const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0xbd93f9, wireframe: true } );
-const torusKnot = new THREE.Mesh( geometry, material );
+const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xbd93f9,
+  wireframe: true,
+});
+const torusKnot = new THREE.Mesh(geometry, material);
 
-let xPos = THREE.MathUtils.randFloat(-40, 40);
-let zPos = THREE.MathUtils.randFloat(-40, 0);
-let yPos = THREE.MathUtils.randFloat(-40, 40);
-torusKnot.position.set(xPos, yPos, zPos);
+// let xPos = THREE.MathUtils.randFloat(-40, 40);
+// let zPos = THREE.MathUtils.randFloat(-40, 0);
+// let yPos = THREE.MathUtils.randFloat(-40, 40);
+// torusKnot.position.set(xPos, yPos, zPos);
 
-scene.add( torusKnot );
+scene.add(torusKnot);
 
+/**
+ * The main animate loop
+ */
 function animate() {
   requestAnimationFrame(animate);
 
   torusKnot.rotation.x += 0.01;
-  torusKnot.rotation.y += 0.005;
-  torusKnot.rotation.z += 0.01;
+  torusKnot.rotation.y += 0.02;
+  torusKnot.rotation.z += 0.03;
 
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
-animate()
+animate();
